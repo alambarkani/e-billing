0c118f0cf555f5e0a60256cbe33366e81ea1801d
Alam Barkani <alambarkani@gmail.com>
2024-06-09 22:55:05 +0700

    Add tailwind and navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.0/dist/cdn.min.js"></script>
+</head>
+
+<body>
+    <div>
+        @include('partials.navbar')
+        @include('partials.sidebar')
+        @yield('content')
+    </div>
+</body>
+
+</html>
