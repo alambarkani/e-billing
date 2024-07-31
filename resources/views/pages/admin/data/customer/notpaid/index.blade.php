cba76fb23c0ebe4ebd40fc88a770a5c85430f62c
Alam Barkani <alambarkani@gmail.com>
2024-07-31 12:49:07 +0700

    update notpaid customer

--- resources/views/pages/admin/data/customer/notpaid/index.blade.php
+++ resources/views/pages/admin/data/customer/notpaid/index.blade.php
@@ -72,29 +72,74 @@
                                         <td class="px-4 py-3">{{ $customer->due_date->day }}</td>
                                         <td class="px-4 py-3">{{ $customer->user->account }}</td>
                                         <td class="px-4 py-3 flex flex-col items-center gap-1 justify-center">
-                                            <a href="{{ route('admin.datas.customer.edit', ['customer' => $customer->id]) }}"
-                                                class="font-medium text-white bg-blue-500 px-2 py-1 rounded border hover:bg-blue-800">Edit</a>
-                                            <button data-modal-target="cust-delete-modal-{{ $customer->id }}"
-                                                data-modal-toggle="cust-delete-modal-{{ $customer->id }}" type="button"
-                                                class="font-medium text-white bg-red-500 px-2 py-1 rounded border hover:bg-red-800">Delete</button>
-                                            {{-- Delete Modal --}}
-                                            @include('components.modals.delete', [
-                                                'item' => $customer,
-                                                'tab' => 'cust',
-                                                'routing' => route('admin.datas.customer.destroy', [
-                                                    'customer' => $customer->id,
-                                                ]),
-                                            ])
+                                            <button data-modal-target="acc-paid-modal-{{ $customer->id }}"
+                                                data-modal-toggle="acc-paid-modal-{{ $customer->id }}" type="button"
+                                                class="font-medium text-white bg-green-500 px-2 py-1 rounded border hover:bg-green-800">Sudah
+                                                Bayar</button>
+                                            {{-- Acc Modal --}}
+                                            <div id="acc-paid-modal-{{ $customer->id }}" tabindex="-1"
+                                                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                                <div class="relative p-4 w-full max-w-md max-h-full">
+                                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                        <button type="button"
+                                                            class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                                            data-modal-hide="acc-paid-modal-{{ $customer->id }}">
+                                                            <svg class="w-3 h-3" aria-hidden="true"
+                                                                xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                viewBox="0 0 14 14">
+                                                                <path stroke="currentColor" stroke-linecap="round"
+                                                                    stroke-linejoin="round" stroke-width="2"
+                                                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                            </svg>
+                                                            <span class="sr-only">Close modal</span>
+                                                        </button>
+                                                        <div class="p-4 md:p-5 text-center">
+                                                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 20 20">
+                                                                <path stroke="currentColor" stroke-linecap="round"
+                                                                    stroke-linejoin="round" stroke-width="2"
+                                                                    d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                            </svg>
+                                                            <h3
+                                                                class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                                                Yakin
+                                                                <span
+                                                                    class="text-blue-800 font-bold">{{ $customer->role ?? ($customer->user->role ?? '') }}</span>
+                                                                <span
+                                                                    class="text-blue-600">{{ $customer->name ?? ($customer->user->name ?? ($customer->id ?? '')) }}</span>
+                                                                Sudah melakukan pembayaran?
+                                                            </h3>
+                                                            <form
+                                                                action="{{ route('admin.datas.customer.paid.confirm', ['customer' => $customer->id]) }}"
+                                                                method="POST">
+                                                                @csrf
+                                                                <button
+                                                                    data-modal-hide="acc-paid-modal-{{ $customer->id }}"
+                                                                    type="submit"
+                                                                    class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                                    Iya, Acc
+                                                                </button>
+                                                            </form>
+                                                            <button data-modal-hide="acc-paid-modal-{{ $customer->id }}"
+                                                                type="button"
+                                                                class="py-2.5 px-5 text-sm font-medium text-gray-900 hover:underline hover:text-blue-700">Cancel</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                                            <a href="{{ route('admin.datas.customer.show', ['customer' => $customer->id]) }}"
-                                                class="font-medium text-blue-600 hover:underline">Details</a>
                                         </td>
                                     </tr>
                                 @endforeach
                             @else
-                                <div class="alert alert-danger">
-                                    Data belum ada
-                                </div>
+                                <tr>
+                                    <td colspan="12"
+                                        class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+                                        role="alert">
+                                        Data belum ada
+                                    </td>
+                                </tr>
                             @endif
                         </tbody>
                     </table>
