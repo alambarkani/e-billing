3dad6a04857ac6cfa190efbf83d09e8f68808ac7
Alam Barkani <alambarkani@gmail.com>
2024-07-31 12:49:40 +0700

    Update Gangguan

--- resources/views/pages/admin/ticket/gangguan.blade.php
+++ resources/views/pages/admin/ticket/gangguan.blade.php
@@ -20,8 +20,9 @@
                 @csrf
 
                 <div>
-                    <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                    <select id="role" name="role" required
+                    <label for="name"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pelanggan</label>
+                    <select id="name" name="name" required
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         <option selected disabled class="bg-gray-200">Select Pelanggan</option>
                         @foreach ($customers as $customer)
@@ -33,12 +34,12 @@
 
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div class="sm:col-span-2">
-                        <label for="name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-                        <input type="text" name="name" id="name" required value="{{ old('name') }}"
+                        <label for="description"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi Gangguan</label>
+                        <input type="text" name="description" id="description" required value="{{ old('description') }}"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder="Nama">
-                        @error('name')
+                        @error('description')
                             <div>{{ $message }}</div>
                         @enderror
                     </div>
